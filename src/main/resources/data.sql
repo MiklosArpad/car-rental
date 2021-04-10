fff6a7bcf85b03d7cf6f248409608270fbe83822
MiklosAprad <miklosarpad2007@gmail.com>
2021-04-10 15:24:29 +0200

    generating db schema.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO vehicles (license_plate, color, make, model) VALUES ('ABC-123', 'piros', 'BMW', 'E36');
+INSERT INTO vehicles (license_plate, color, make, model) VALUES ('CBA-321', 'kék', 'Mercedes', 'GLA');
+INSERT INTO vehicles (license_plate, color, make, model) VALUES ('XYZ-987', 'sárga', 'Audi', 'A8');
